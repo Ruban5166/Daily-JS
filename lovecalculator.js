eb29a0d14379236786debb45e2b6d64585dd6723
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2025-07-28 12:20:57 +0530

    Update lovecalculator.js

--- lovecalculator.js
+++ lovecalculator.js
@@ -6,9 +6,14 @@ var n = Math.random();
 var lovePercentage = Math.floor(n *100)+1;
 
 if(lovePercentage > 70){
+    alert("Your love percentage is:"+lovePercentage + "your love with yathis and yathis");
+}
+
+else if(lovePercentage > 30 && lovePercentage <= 70){
     alert("Your love percentage is:"+lovePercentage + "your made for each other");
 }
+    
 else{
 
-    alert("You love percentage is:" + lovePercentage);
+    alert("You love percentage is:" + lovePercentage + "you both are going together oil and water");
 }
