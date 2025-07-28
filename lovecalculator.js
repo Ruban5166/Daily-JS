0b1e3f3766afd6922cc31f897d62313ef4edfa70
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2025-07-28 12:11:50 +0530

    Update lovecalculator.js

--- lovecalculator.js
+++ lovecalculator.js
@@ -5,5 +5,10 @@ var partnerName = prompt("Enter Your Partnername:");
 var n = Math.random();
 var lovePercentage = Math.floor(n *100)+1;
 
+if(lovePercentage > 70){
+    alert("Your love percentage is:"+lovePercentage + "your made for each other");
+}
+else{
 
-alert("Your love percentage is:"+lovePercentage);
+    alert("You love percentage is:" + lovePercentage);
+}
