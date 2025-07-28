72683fa63e05c3e8d153d30944779918e4b9cc68
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2025-07-28 12:02:05 +0530

    Create lovecalculator.js

---
+++ lovecalculator.js
@@ -0,0 +1,9 @@
+var yourName = prompt("Enter Your name:");
+var partnerName = prompt("Enter Your Partnername:");
+
+
+var n = Math.random();
+var lovePercentage = Math.floor(n *100)+1;
+
+
+alert("Your love percentage is:"+lovePercentage);
