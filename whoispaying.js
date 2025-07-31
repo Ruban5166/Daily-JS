aec925fe7b95186fd41748ed66454f0ed7056605
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2025-07-31 12:33:03 +0530

    Create whoispaying.js

---
+++ whoispaying.js
@@ -0,0 +1,20 @@
+
+var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    
+    var num = Math.floor(Math.random() * names.length);
+
+    
+  return names[num] + " is going to buy lunch today!";
+
+
+/******Don't change the code below*******/    
+}
+
+
+whosPaying(names);
